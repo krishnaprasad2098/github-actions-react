47ff57e587bb8d206f504b8a3847dc02c7d640a9
krishnaprasad2098 <payamma.krishna@gmail.com>
2025-09-09 09:37:01 +0530

    remove unsed imports

--- src/tests/Button.test.tsx
+++ src/tests/Button.test.tsx
@@ -1,5 +1,4 @@
-// src/components/__tests__/Button.test.tsx
-import React from 'react';
+
 import { render, screen, fireEvent } from '@testing-library/react';
 import Button from '../components/Button';
 
